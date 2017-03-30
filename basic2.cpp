bbdd727b17d1cf7389c858cdd0b84d19c05ba195
clairetsai818 <clairetsai818@gmail.com>
2017-03-30 10:18:06 +0800

    question2

---
+++ basic2.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+    int cnt = 0;
+    cin >> input;
+    for (int i = 1;i <= input;i++) {
+        if (i % 15 == 0) {
+            cnt ++;
+            cout << i << " ";
+        }
+        else if ((i % 5 == 0) || (i % 3 ==0)) {
+            continue;
+        }
+        else {
+            cnt++;
+            cout << i << " ";
+        }
+    }
+    cout << cnt;
+}
